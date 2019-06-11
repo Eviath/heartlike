c83c30170300fe33f41cec74bd7568f151182337
Jacek <eviathos@gmail.com>
2019-06-12 00:46:18 +0200

    articles styling, more locales, readme update

---
+++ spec/dummy/db/migrate/20190611191247_create_heartlike_articles.heartlike.rb
@@ -0,0 +1,16 @@
+# This migration comes from heartlike (originally 20190610130007)
+class CreateHeartlikeArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :heartlike_articles do |t|
+      # Base of article
+      t.string :title
+      t.text :body
+      t.references :user, null: false, foreign_key: true
+
+      # Hearts counter =counter_cache=
+      t.integer :hearts_count, default: 0
+
+      t.timestamps
+    end
+  end
+end
